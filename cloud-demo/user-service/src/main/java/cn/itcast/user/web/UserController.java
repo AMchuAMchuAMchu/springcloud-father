bd717f56ca63ea7f032bbe3f112de2030d7d393e
AMchuAMchuAMchu <2466130840@qq.com>
2022-12-21 18:07:33 +0800

    shiina

--- cloud-demo/user-service/src/main/java/cn/itcast/user/web/UserController.java
+++ cloud-demo/user-service/src/main/java/cn/itcast/user/web/UserController.java
@@ -18,7 +18,7 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @Value("{pattern.dateformat}")
+    @Value("${pattern.dateformat}")
     private String format;
 
     /**
