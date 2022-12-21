d1f119a8df561631819497707ae1484fd5ddbf42
AMchuAMchuAMchu <2466130840@qq.com>
2022-12-21 10:41:44 +0800

    shiina

---
+++ cloud-demo/eureka-server/src/main/java/com/itcast/EurekaApplication.java
@@ -0,0 +1,21 @@
+package com.itcast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Description ==> TODO
+ * BelongsProject ==> springcloud-father
+ * BelongsPackage ==> com.itcast
+ * Version ==> 1.0
+ * CreateTime ==> 2022-12-21 09:52:50
+ * Author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_制作委员会_start
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
