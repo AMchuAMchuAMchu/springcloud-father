eb0c816ee091da0c11c0416a2770c06bf6bbdb4e
AMchuAMchuAMchu <2466130840@qq.com>
2022-12-21 18:04:56 +0800

    shiina

--- cloud-demo/user-service/src/main/java/cn/itcast/user/web/UserController.java
+++ cloud-demo/user-service/src/main/java/cn/itcast/user/web/UserController.java
@@ -4,7 +4,11 @@ import cn.itcast.user.pojo.User;
 import cn.itcast.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Slf4j
 @RestController
@@ -13,6 +17,9 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @Value("{pattern.dateformat}")
+    private String format;
 
     /**
      * 路径： /user/110
@@ -24,4 +31,10 @@ public class UserController {
     public User queryById(@PathVariable("id") Long id) {
         return userService.queryById(id);
     }
+
+    @GetMapping("/now")
+    public String now(){
+        return DateTimeFormatter.ofPattern(format).format(LocalDateTime.now());
+    }
+
 }
