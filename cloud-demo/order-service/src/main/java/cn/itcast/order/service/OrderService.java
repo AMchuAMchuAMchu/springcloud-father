c7a90fc28d7d2cb55c35b3bb4b27116fd97f4dad
AMchuAMchuAMchu <2466130840@qq.com>
2022-12-21 09:31:35 +0800

    shiina

--- cloud-demo/order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ cloud-demo/order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -20,7 +20,7 @@ public class OrderService {
         // 1.查询订单
         Order order = orderMapper.findById(orderId);
 
-        String url = "http://localhost:8080/"+order.getUserId();
+        String url = "http://localhost:8081/user/"+order.getUserId();
 
         User user = restTemplate.getForObject(url, User.class);
 
