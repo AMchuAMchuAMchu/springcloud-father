e3346fd761aaadff04f8f4fa539220e0d04b7dd1
AMchuAMchuAMchu <2466130840@qq.com>
2022-12-21 08:52:30 +0800

    shiina

--- cloud-demo/order-service/src/main/java/cn/itcast/order/OrderApplication.java
+++ cloud-demo/order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @MapperScan("cn.itcast.order.mapper")
 @SpringBootApplication
@@ -12,5 +13,11 @@ public class OrderApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class, args);
     }
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
 
 }
--- cloud-demo/order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ cloud-demo/order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -2,8 +2,10 @@ package cn.itcast.order.service;
 
 import cn.itcast.order.mapper.OrderMapper;
 import cn.itcast.order.pojo.Order;
+import cn.itcast.order.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class OrderService {
@@ -11,9 +13,19 @@ public class OrderService {
     @Autowired
     private OrderMapper orderMapper;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     public Order queryOrderById(Long orderId) {
         // 1.查询订单
         Order order = orderMapper.findById(orderId);
+
+        String url = "http://localhost:8080/"+order.getUserId();
+
+        User user = restTemplate.getForObject(url, User.class);
+
+        order.setUser(user);
+
         // 4.返回
         return order;
     }
