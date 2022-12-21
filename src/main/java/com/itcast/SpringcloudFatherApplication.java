1f0741c04d80209550808b61716f87e57636110a
AMchuAMchuAMchu <2466130840@qq.com>
2022-12-21 08:22:36 +0800

    shiina

---
+++ src/main/java/com/itcast/SpringcloudFatherApplication.java
@@ -0,0 +1,13 @@
+package com.itcast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudFatherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudFatherApplication.class, args);
+    }
+
+}
